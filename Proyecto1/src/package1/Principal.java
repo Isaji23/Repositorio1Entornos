796378db592477d8ff9114304e6d9f1c461ee7cb
usuario <usuario@DESKTOP-0VSBK2V>
2023-05-03 08:57:22 +0200

    Segundo commit

--- Proyecto1/src/package1/Principal.java
+++ Proyecto1/src/package1/Principal.java
@@ -3,7 +3,8 @@ package package1;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Control de versiones con git");
+		System.out.println("Primer cambio");
 
 	}
 
